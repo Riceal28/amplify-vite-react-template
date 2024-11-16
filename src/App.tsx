e8b5ef1921504e2ab66635655510ce9d183548f1
Riceal28 <2908687620@qq.com>
2024-11-16 21:59:54 +0800

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
-import { useAuthenticator } from '@aws-amplify/ui-react';
+import { useAuthenticator } from '@aws-amplify/ui-react';// setp 6 login function part4-for sign out
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
 function App() {
-  const { signOut } = useAuthenticator();
+  
+  const { user, signOut } = useAuthenticator();// setp 7 login function part5-for sign out
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   
-  function deleteTodo(id: string) {
+  function deleteTodo(id: string) { // setp 1 delete function part1
     client.models.Todo.delete({ id })
   }
 
@@ -23,16 +24,19 @@ function App() {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
+  
+
 
   return (
+ 
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
           <li
           
-          onClick={() => deleteTodo(todo.id)}
+          onClick={() => deleteTodo(todo.id)} //setp 2 delete function part2
 
           key={todo.id}>{todo.content}</li>
         ))}
@@ -44,8 +48,9 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
+      <button onClick={signOut}>Sign out</button> 
+    </main> // setp 8 login function part6-for sign out
+
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react'; // setp 3 login function part1
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import '@aws-amplify/ui-react/styles.css';
+import '@aws-amplify/ui-react/styles.css';// setp 4 login function part2
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  
+  // setp 5 login function part3
   <React.StrictMode>
     <Authenticator>
       <App />
